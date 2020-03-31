c57b6694c7e09f2807bfd6ee8c6d982e862bf969
Crocodylle <souza89.teleco@gmail.com>
2020-03-31 18:33:47 +1100

    Controlando as pecas capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -11,17 +11,19 @@ namespace xadrez_console
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
-
-                Tela.imprimirTabuleiro(partida.tab);
+                               
                 while (!partida.terminada)
                 {
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno" + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
+
+
+                    //    Tela.imprimirTabuleiro(partida.tab);
+                    //    Console.WriteLine();
+                    //    Console.WriteLine("Turno" + partida.turno);
+                    //    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
                         Console.WriteLine();
                         Console.Write("Origem ");
@@ -36,6 +38,7 @@ namespace xadrez_console
                         Console.WriteLine();
                         Console.Write("Destino: ");
                         Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
 
                         partida.executaMovimento(origem, destino);
                     }
